94ab8c6c41346b3404ca7da20b6e5932455948d8
moise10r <moiserushanika2006@gmail.com>
2021-10-03 21:58:45 +0200

    Expose header to the client

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cors());
 app.use(function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.header("Access-Control-Allow-Headers", "x-auth-token");
 	next();
 });
 require("./routes/router")(app);

b84e9add8e9cc12e125464c6b04c52af80986dcd
Barack Mukelenga <barackmukelenga100@gmail.com>
2021-10-09 02:03:37 +0200

    Add the status property to the user's schema

--- models/doctor.js
+++ models/doctor.js
@@ -1,66 +1,71 @@
-const mongoose = require("mongoose");
-const Joi = require("@hapi/joi");
+const mongoose = require('mongoose');
+const Joi = require('@hapi/joi');
 
 const doctorsSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: true,
-		minlength: 3,
-		maxlength: 50,
-	},
-	lastName: {
-		type: String,
-		minlength: 3,
-		maxlength: 50,
-	},
-	email: {
-		type: String,
-		required: true,
-		minlength: 5,
-		maxlength: 255,
-		unique: true,
-	},
-	role: {
-		type: String,
-	},
-	password: {
-		type: String,
-		required: true,
-		minlength: 5,
-		maxlength: 1024,
-	},
-	phoneNumber: {
-		type: String,
-	},
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 50,
+  },
+  lastName: {
+    type: String,
+    minlength: 3,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+    unique: true,
+  },
+  role: {
+    type: String,
+  },
+  status: {
+    type: String,
+    enum: ['active', 'banned', 'pending'],
+    default: 'pending',
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 1024,
+  },
+  phoneNumber: {
+    type: String,
+  },
 
-	profileImage: {
-		type: String,
-	},
-	isSuperAdmin: {
-		type: Boolean,
-		default: false,
-	},
-	isAdmin: {
-		type: Boolean,
-		default: false,
-	},
-	createdAt: {
-		type: String,
-	},
+  profileImage: {
+    type: String,
+  },
+  isSuperAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: String,
+  },
 });
 
-const Doctor = mongoose.model("doctors", doctorsSchema);
+const Doctor = mongoose.model('doctors', doctorsSchema);
 function validateDoctor(doctor) {
-	const schema = Joi.object().keys({
-		name: Joi.string().min(2).max(50).required(),
-		lastName: Joi.string().min(2).max(50),
-		email: Joi.string().min(2).max(50).required().email(),
-		role: Joi.string().min(5).max(255),
-		password: Joi.string().min(5).max(255).required(),
-		phoneNumber: Joi.string(),
-		profileImage: Joi.string(),
-	});
-	return schema.validate(doctor);
+  const schema = Joi.object().keys({
+    name: Joi.string().min(2).max(50).required(),
+    lastName: Joi.string().min(2).max(50),
+    email: Joi.string().min(2).max(50).required().email(),
+    role: Joi.string().min(5).max(255),
+    password: Joi.string().min(5).max(255).required(),
+    phoneNumber: Joi.string(),
+    profileImage: Joi.string(),
+  });
+  return schema.validate(doctor);
 }
 exports.validateDoctor = validateDoctor;
 exports.Doctor = Doctor;
--- models/user.js
+++ models/user.js
@@ -1,83 +1,91 @@
-const mongoose = require("mongoose");
-const Joi = require("@hapi/joi");
+const mongoose = require('mongoose');
+const Joi = require('@hapi/joi');
 const usersSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: true,
-		minlength: 3,
-		maxlength: 50,
-	},
-	lastName: {
-		type: String,
-		minlength: 3,
-		maxlength: 50,
-	},
-	email: {
-		type: String,
-		required: true,
-		minlength: 5,
-		maxlength: 255,
-		unique: true,
-	},
-	password: {
-		type: String,
-		required: true,
-		minlength: 5,
-		maxlength: 1024,
-	},
-	phoneNumber: {
-		type: String,
-	},
-	age: {
-		type: String,
-	},
-	profileImage: {
-		type: String,
-	},
-	gender: {
-		type: String,
-	},
-	country: {
-		type: String,
-		maxlength: 50,
-	},
-	city: {
-		type: String,
-		maxlength: 50,
-	},
-	streetNumber: {
-		type: String,
-		maxlength: 1000,
-	},
-	isSuperAdmin: {
-		type: Boolean,
-		default: false,
-	},
-	isAdmin: {
-		type: Boolean,
-		default: false,
-	},
-	createdAt: {
-		type: String,
-	},
+  name: {
+    type: String,
+    required: true,
+    minlength: 3,
+    maxlength: 50,
+  },
+  lastName: {
+    type: String,
+    minlength: 3,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+    unique: true,
+  },
+  status: {
+    type: String,
+    enum: ['active', 'inactive', 'pending'],
+    default: 'pending',
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 1024,
+  },
+  phoneNumber: {
+    type: String,
+  },
+  kitIdentifier: {
+    type: String,
+  },
+  age: {
+    type: String,
+  },
+  profileImage: {
+    type: String,
+  },
+  gender: {
+    type: String,
+  },
+  country: {
+    type: String,
+    maxlength: 50,
+  },
+  city: {
+    type: String,
+    maxlength: 50,
+  },
+  streetNumber: {
+    type: String,
+    maxlength: 1000,
+  },
+  isSuperAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: String,
+  },
 });
 
-const User = mongoose.model("users", usersSchema);
+const User = mongoose.model('users', usersSchema);
 function validateUser(user) {
-	const schema = Joi.object().keys({
-		name: Joi.string().max(50).required(),
-		email: Joi.string().max(50).required().email(),
-		password: Joi.string().min(5).max(255).required(),
-		lastName: Joi.string().max(50),
-		phoneNumber: Joi.string(),
-		age: Joi.number().min(5).max(255),
-		profileImage: Joi.string(),
-		gender: Joi.string(),
-		country: Joi.string().max(50),
-		city: Joi.string().max(50),
-		streetNumber: Joi.number().max(1000),
-	});
-	return schema.validate(user);
+  const schema = Joi.object().keys({
+    name: Joi.string().max(50).required(),
+    email: Joi.string().max(50).required().email(),
+    password: Joi.string().min(5).max(255).required(),
+    lastName: Joi.string().max(50),
+    phoneNumber: Joi.string(),
+    age: Joi.number().min(5).max(255),
+    profileImage: Joi.string(),
+    gender: Joi.string(),
+    country: Joi.string().max(50),
+    city: Joi.string().max(50),
+    streetNumber: Joi.number().max(1000),
+  });
+  return schema.validate(user);
 }
 exports.validateUser = validateUser;
 exports.User = User;

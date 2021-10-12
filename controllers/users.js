5b3a8c90343351ac7799539767c16d2f052bb68a
Barack Mukelenga <barackmukelenga100@gmail.com>
2021-10-12 12:53:41 +0200

    Fix the bug in users controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const {
-  Doctor,
-  validateDoctor,
-  validateDoctorEdit,
-} = require('../models/doctor');
-const { Conversation } = require('../models/conversation');
-const { User } = require('../models/user');
-const { Message } = require('../models/message');
+const { User, validateUser, validateUserEdit } = require('../models/user');
 const bcrypt = require('bcrypt');
+const { Conversation } = require('../models/conversation');
+const { Message } = require('../models/message');
+const { Appointment } = require('../models/appointment');
+const Fawn = require('fawn');
+const mongoose = require('mongoose');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
 const moment = require('moment');
@@ -16,145 +14,164 @@ const auth = require('../middlewares/auth');
 const admin = require('../middlewares/admin');
 const superAdmin = require('../middlewares/superAdmin');
 
-// auth.verifyToken,
-// admin,
-router.post('/', [auth.verifyToken, admin], async (req, res) => {
-  let {
-    name,
-    lastName,
-    email,
-    password,
-    phoneNumber,
-    age,
-    profileImage,
-    gender,
-    country,
-    status,
-    city,
-    about,
-  } = req.body;
-  const validation = validateDoctor(req.body);
+Fawn.init(mongoose);
+//, [auth.verifyToken, admin]
+router.post('/', async (req, res) => {
+  const { name, email, password } = req.body;
+  const validation = validateUser(req.body);
   if (validation.error) {
-    res
-      .header('x-auth-token')
-      .status(400)
-      .send(validation.error.details[0].message);
+    res.status(400).send(validation.error.details[0].message);
     return;
   }
-  let doctor = await Doctor.findOne({
-    $or: [
-      {
-        email,
-      },
-    ],
-  });
-  if (doctor)
-    return res
-      .header('x-auth-token')
-      .status(400)
-      .send('doctor already registered.');
-  doctor = new Doctor({
+  let user = await User.findOne({ email });
+
+  if (user) return res.status(400).send('User already registered.');
+  user = new User({
     name,
     email,
     password,
-    lastName,
-    phoneNumber,
-    age,
-    profileImage,
-    gender,
-    country,
-    about,
-    city,
-    status,
+    lastName: '',
+    phoneNumber: '',
+    age: '',
+    profileImage: '',
+    gender: '',
+    country: '',
+    city: '',
+    streetNumber: '',
+    isAdmin: false,
+    isSuperAdmin: false,
     createdAt: moment(Date.now()).format('LL'),
   });
   bcrypt.genSalt(10, (err, salt) => {
-    bcrypt.hash(doctor.password, salt, (err, hash) => {
-      if (err) throw err;
-      doctor.password = hash;
+    bcrypt.hash(user.password, salt, (err, hash) => {
+      if (err) return res.send('password is incorrect');
+      user.password = hash;
       try {
-        doctor.save();
+        new Fawn.Task().save('users', user).run();
         const payload = {
-          _id: doctor._id,
-          name: doctor.name,
-          email: doctor.email,
-          isAdmin: doctor.isAdmin,
-          isSuperAdmin: doctor.isSuperAdmin,
-          lastName: doctor.lastName,
-          phoneNumber: doctor.phoneNumber,
-          age: doctor.age,
-          profileImage: doctor.profileImage,
-          gender: doctor.gender,
-          country: doctor.country,
-          about: doctor.about,
-          city: doctor.city,
-          status: doctor.status,
-          isDoctor: doctor.isDoctor,
+          _id: user._id,
+          name: user.name,
+          email: user.email,
+          lastName: '',
+          phoneNumber: '',
+          age: '',
+          profileImage: '',
+          gender: '',
+          country: '',
+          city: '',
+          streetNumber: '',
+          status: user.status,
+          kitIdentifier: '',
         };
         const token = jwt.sign(payload, process.env.SECRET_TOKEN_KEY);
         return res.send(token);
       } catch (error) {
-        res.header('x-auth-token').send('Something went wrong');
+        res.send('something went wrong ');
       }
     });
   });
 });
 
-// auth.verifyToken,
-router.put('/:id', [auth.verifyToken, admin], async (req, res) => {
+router.delete('/:id', [auth.verifyToken, admin], async (req, res) => {
+  const user = await User.findOneAndDelete({ _id: req.params.id });
+  if (!user)
+    return res.status(404).send('The use with the given ID was not found.');
+  // remove all the conversations where the user is among the participants
+  await Appointment.deleteMany({
+    patient: {
+      $in: [user._id],
+    },
+  });
+  // delete all the conversations where the doctor is among participants
+  await Conversation.deleteMany({
+    participants: {
+      $elemMatch: {
+        _id: user._id,
+      },
+    },
+  });
+
+  // delete all the messages sent by the doctor
+  await Message.deleteMany({
+    sender: {
+      $elemMatch: {
+        _id: user._id,
+      },
+    },
+  });
+
+  res.send(user);
+});
+
+router.get('/', async (req, res) => {
+  const users = await User.find();
+  if (!users) return res.status(404).send('There is no user ');
+  res.send(users);
+});
+
+router.get('/:id', async (req, res) => {
+  const user = await User.findById({ _id: req.params.id });
+  if (!user)
+    return res.status(404).send('The User with this ID was not found. ');
+  res.status(200).send(user);
+});
+
+router.put('/:id', [auth.verifyToken], async (req, res) => {
   const {
     name,
     lastName,
     email,
+    password,
     phoneNumber,
     age,
     profileImage,
     gender,
     country,
     status,
+    kitIdentifier,
     city,
-    about,
+    streetNumber,
   } = req.body;
-  const validation = validateDoctorEdit(req.body);
+  const validation = validateUserEdit(req.body);
   if (validation.error) {
     return res.status(400).send(validation.error.details[0].message);
   }
 
-  let password = req.body.password;
   if (password) {
     const salt = await bcrypt.genSalt(10);
     const hash = await bcrypt.hash(password, salt);
     password = password = hash;
   }
-  let doctor = await Doctor.findById(req.params.id);
-  if (!doctor)
-    return res.status(404).send('The Doctor with this ID was not found. ');
+  let user = await User.findById(req.params.id);
+  if (!user)
+    return res.status(404).send('The User with this ID was not found. ');
 
-  doctor = await Doctor.findByIdAndUpdate(
+  user = await User.findByIdAndUpdate(
     { _id: req.params.id },
     {
       $set: {
         name,
         lastName,
         email,
-        password: password ? password : doctor.password,
+        password: password ? password : user.password,
         phoneNumber,
         age,
         gender,
-        about,
         profileImage,
         status,
+        kitIdentifier,
+        streetNumber,
         city,
         country,
       },
     },
     { new: true },
   );
-  if (!doctor)
-    return res.status(404).send('The Doctor with this ID was not found. ');
+  if (!user)
+    return res.status(404).send('The User with this ID was not found. ');
 
   const token = jwt.sign(
-    _.pick(doctor, [
+    _.pick(user, [
       '_id',
       'name',
       'email',
@@ -164,10 +181,10 @@ router.put('/:id', [auth.verifyToken, admin], async (req, res) => {
       'profileImage',
       'gender',
       'country',
-      'about',
       'city',
+      'streetNumber',
       'status',
-      'isDoctor',
+      'kitIdentifier',
     ]),
     process.env.SECRET_TOKEN_KEY,
   );
@@ -175,57 +192,7 @@ router.put('/:id', [auth.verifyToken, admin], async (req, res) => {
   res.send(token);
 });
 
-router.delete('/:id', [auth.verifyToken, admin], async (req, res) => {
-  const doctor = await Doctor.findOneAndDelete({ _id: req.params.id });
-  if (!doctor)
-    return res
-      .header('x-auth-token')
-      .status(404)
-      .send('The use with the given ID was not found.');
-  // delete all the appointments where the doctor._id == doctor._id
-  await Appointment.deleteMany({
-    doctor: {
-      $in: [doctor._id],
-    },
-  });
-  // delete all the conversations where the doctor is among participants
-  await Conversation.deleteMany({
-    participants: {
-      $elemMatch: {
-        _id: doctor._id,
-      },
-    },
-  });
-
-  // delete all the messages sent by the doctor
-  await Message.deleteMany({
-    sender: {
-      $elemMatch: {
-        _id: doctor._id,
-      },
-    },
-  });
-
-  res.header('x-auth-token').send(doctor);
-});
-
-router.get('/', [auth.verifyToken], async (req, res) => {
-  const doctors = await Doctor.find();
-  if (!doctors)
-    return res.header('x-auth-token').status(404).send('There is no doctor ');
-  res.header('x-auth-token').send(doctors);
-});
-
-router.get('/:id', async (req, res) => {
-  const doctor = await Doctor.findById({ _id: req.params.id });
-  if (!doctor)
-    return res
-      .header('x-auth-token')
-      .status(404)
-      .send('The doctor with this ID was not found. ');
-  res.header('x-auth-token').status(200).send(doctor);
-});
-
+//make user Admin
 router.put('/:id/admin', [auth.verifyToken, admin], superAdmin.isSuperAdmin);
 
 module.exports = router;
